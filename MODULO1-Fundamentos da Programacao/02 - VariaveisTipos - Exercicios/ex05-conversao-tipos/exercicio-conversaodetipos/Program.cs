ec0bc37e8ee1edb6646bc71da5624ff46feb763e
Carlos Nascimento <sysjornada@hotmail.com>
2026-02-26 16:43:04 -0300

    Resolução exercicio 5

---
+++ MODULO1-Fundamentos da Programacao/02 - VariaveisTipos - Exercicios/ex05-conversao-tipos/exercicio-conversaodetipos/Program.cs
@@ -0,0 +1,8 @@
+﻿decimal temperaturaCelsius = 26.8m;
+
+//calcule
+
+double temperaturaFahrenhet = (decimal.ToDouble(temperaturaCelsius) * 9 / 5) + 32;
+int temperaturaInteira = temperaturaCelsius > 0 ? (int)temperaturaCelsius : (int)temperaturaCelsius - 1;
+
+Console.WriteLine($"A temperatura de {temperaturaCelsius}°C corresponde a {temperaturaFahrenhet}°F. A parte interira em Celsius é {temperaturaInteira}°C ");
