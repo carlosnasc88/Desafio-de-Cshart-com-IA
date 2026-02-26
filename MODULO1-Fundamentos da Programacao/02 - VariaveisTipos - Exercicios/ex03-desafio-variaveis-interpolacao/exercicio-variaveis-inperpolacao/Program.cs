933af1ce7c7ac691a6e004752f571bdb77b1ea29
Carlos Nascimento <sysjornada@hotmail.com>
2026-02-26 12:50:26 -0300

    Implentação e resloção dos exercicios de variaveis-interpolação

--- MODULO1-Fundamentos da Programacao/02 - VariaveisTipos - Exercicios/ex03-desafio-variaveis-interpolacao/exercicio-variaveis-inperpolacao/Program.cs
+++ MODULO1-Fundamentos da Programacao/02 - VariaveisTipos - Exercicios/ex03-desafio-variaveis-interpolacao/exercicio-variaveis-inperpolacao/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿string nome = "Carlos Nascimento";
+int idade = 28;
+double altura = 1.75;
+string profissao = "Desenvolvedor C#| .Net| Full Stack";
+decimal salarioDesejado = 13500.00m;
+
+Console.WriteLine($"Meu nome é {nome}, tenho {idade} anos de idade, tenho {altura}m de altura, e quero trabalhar como {profissao} e meu salário desejado é de R${salarioDesejado}");
+Console.WriteLine("--------------------------------");
+Console.WriteLine($"Se eu ganhar R${salarioDesejado} por mês, em 12 meses eu vou ganhar R${salarioDesejado *12}");
