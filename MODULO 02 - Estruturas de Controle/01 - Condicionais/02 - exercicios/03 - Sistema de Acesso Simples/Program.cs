949f8ad0baa652aae0ef34501b580b5a5edff166
Carlos Nascimento <sysjornada@hotmail.com>
2026-03-15 12:48:18 -0300

    Resolucao exercicios modulo2

---
+++ MODULO 02 - Estruturas de Controle/01 - Condicionais/02 - exercicios/03 - Sistema de Acesso Simples/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(@"Sistema de Acesso Simples
+1 - Administrador
+2 - Gerente
+3 - Funcionário
+4 - Visitante
+0 - Sair");
+int tipoPerfil = Convert.ToInt16(Console.ReadLine());
+switch(tipoPerfil)
+{
+    case 1: Console.WriteLine("**Administrado**: Bem-vindo, Administrador. Você tem acesso total Acesso total ao sistema");break;
+    case 2: Console.WriteLine("**Gerente**: Bem-vindo Gerete, você tem acesso a relatórios e cadastros do sistema");break;
+    case 3: Console.WriteLine("**Funcionário**: Bem-vindo Funcionário, você tem aaesso básico ás funcionalidade do sistema" );break;
+    case 4: Console.WriteLine("**Visitante**: Bem-vindo Visitante, você te acesso somente á vissualização de informacões públicas");break;
+    case 0: Console.WriteLine("Saindo do sistema...");return;
+    default: Console.WriteLine("Perfil invalido");break;
+
+}
